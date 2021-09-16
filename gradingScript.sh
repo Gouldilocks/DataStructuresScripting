31d7b7321edf9c6141dc3c62bd56743a820e86f9
Gouldilocks <cagould@smu.edu>
2021-09-16 14:09:57 -0500

    added script

---
+++ gradingScript.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+execname=$1
+# Get the arguments for the executables
+args=''
+echo "RETRIEVING ARGS FROM InputFilesPaths.txt"
+while IFS= read -r line; do
+    args+=" $line"
+    echo "Text read from file: $line"
+done < InputFilePaths.txt
+#args includes a space at the beginning, so when passed as args to executable, no need to add a space then.
+echo "args received are: $args"
+
+# Clone all projects from the students
+sudo rm -r projects
+mkdir projects
+cd projects
+while IFS= read -r line; do
+	echo "Cloning: $line"
+	git clone $line
+done < ../GitRepos.txt
+cd ..
+
+# Run Cmake and Make on all projects, and then run them
+cd projects
+for project in *; do
+echo "Project name: $project"
+cd $project
+cmake .
+make
+start=$(date +%s)
+echo "RUNNING: ./$execname$args"
+./$execname$args
+end=$(date +%s)
+runtime=$(($end-$start))
+echo "Runtime for: $project = $runtime seconds" >> ../../timings.txt
+cd ..
+done
+cd ..
