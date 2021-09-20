1c6a578993fb3924d5c8e0d4adcd6ef9dd689a1d
Gouldilocks <cagould@smu.edu>
2021-09-20 11:19:46 -0500

    modified comments on gradingScript.sh modified names of files

--- gradingScript.sh
+++ gradingScript.sh
@@ -1,43 +1,58 @@
 #!/bin/bash
+
 # Get the arguments for the executables
-args=''
-echo "RETRIEVING ARGS FROM InputFilesPaths.txt"
-while IFS= read -r line; do
-    args+=" $line"
-    echo "Text read from file: $line"
-done < InputFilePaths.txt
-#args includes a space at the beginning, so when passed as args to executable, no need to add a space then.
-echo "args received are: $args"
+    args=''
+    echo "RETRIEVING ARGS FROM args.txt"
+    # Pull from file line by line, adding onto args variable each iteration
+    while IFS= read -r line; do
+        args+=" $line"
+        echo "Text read from file: $line"
+    done < args.txt
+    echo "args received are: $args"
 
 # Clone all projects from the students
-sudo rm -r projects
-mkdir projects
-cd projects
-while IFS= read -r line; do
-	echo "Cloning: $line"
-	git clone $line
-done < ../GitRepos.txt
-cd ..
+    # Remove projects folder if it exists, then replace it
+    sudo rm -r projects
+    mkdir projects
+    cd projects
+    # Pulls from file line by line, then clones the repo pulled from the file
+    while IFS= read -r line; do
+        echo "Cloning: $line"
+        git clone $line
+    done < ../GitRepos.txt
+    cd ..
 
 # Run Cmake and Make on all projects, and then run them
-cd projects
-for project in *; do
-echo "Project name: $project"
-cd $project
-cmake .
-make
-start=$(date +%s)
-workDire=$PWD
-workDire+="/CMakeLists.txt"
-echo "path sent to python is: $workDire"
-python3 ../../main.py "$workDire"
-execname="$(head -1 ../../executableName.txt)"
-echo "found executable name: $execname"
-echo "RUNNING: $PWD/$execname$args"
-./$execname$args
-end=$(date +%s)
-runtime=$(($end-$start))
-echo "Runtime for: $project = $runtime seconds" >> ../../timings.txt
-cd ..
-done
-cd ..
+    cd projects
+    for project in *; do
+    echo "Project name: $project"
+
+    # Change directory into project directory, run cmake and make on the project
+    cd $project
+    cmake .
+    make
+
+    # Parse the cmakelists for the executable name
+    workDire=$PWD
+    workDire+="/CMakeLists.txt"
+    echo "path sent to python is: $workDire"
+    python3 ../../cmakeListsParser.py "$workDire"
+    execname="$(head -1 ../../executableName.txt)"
+    echo "found executable name: $execname"
+
+    # Run the executable found with and without arguments, using start and end for timing info
+    echo "RUNNING: $PWD/$execname$args"
+    start=$(date +%s)
+    ./$execname$args
+    end=$(date +%s)
+    runtime=$(($end-$start))
+
+    # Print runtime to timings.txt
+    echo "Runtime for: $project = $runtime seconds" >> ../../timings.txt
+
+    # Change dir to outside of project directory
+    cd ..
+    done
+
+    # Change dir out of projects/
+    cd ..
